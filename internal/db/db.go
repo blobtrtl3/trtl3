1930d988177f2b0c68199ce218b5677c8373b5a2
nothiaki <celsohenrique367@gmail.com>
2025-08-25 14:32:20 -0300

    feat: duck db

---
+++ internal/db/db.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/marcboeker/go-duckdb/v2"
+)
+
+func NewDbConn() *sql.DB {
+	db, err := sql.Open("db", "")
+	if err != nil {
+		log.Fatalf("Could not create connection in database, reason: %s", err)
+	}
+
+	return db
+}
