1106e07bbf7f1c80f2780b3002b6c8ad23960179
nothiaki <celsohenrique367@gmail.com>
2025-08-27 14:25:07 -0300

    feat: find blobinfo by id

--- internal/usecase/storage/findById.go
+++ internal/usecase/storage/findById.go
@@ -3,7 +3,11 @@ package storage
 import "github.com/blobtrtl3/trtl3/internal/domain"
 
 func (bs *BlobStorage) FindByID(id string) (*domain.BlobInfo, error) {
-	bi := domain.BlobInfo{}
+	var bi domain.BlobInfo
+
+	if err := bs.db.QueryRow("SELECT 1 FROM blobsinfo WHERE id=?)", id).Scan(&bi); err != nil {
+		return nil, err
+	}
 
 	return &bi, nil
 }
