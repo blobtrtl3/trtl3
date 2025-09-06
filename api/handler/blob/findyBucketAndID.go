f68f9fe5c0f742369775abbba44e69fcaacadce1
nothiaki <celsohenrique367@gmail.com>
2025-09-05 22:51:18 -0300

    docs: find unique blob swag

--- api/handler/blob/findyBucketAndID.go
+++ api/handler/blob/findyBucketAndID.go
@@ -7,6 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary      Find unique blob
+// @Description  Find blob by using bucket and id
+// @Accept       json
+// @Produce      json
+// @Param        Authorization header string true "Access token" default("")
+// @Param 			 bucket path string true "Bucket name"
+// @Param 			 id path string true "Blob id"
+// @Success      200 {object} domain.BlobInfo
+// @Router       /blobs/{bucket}/{id} [get]
 func (bh *BlobHandler) FindByBucketAndID(c *gin.Context) {
 	bucket := c.Param("bucket")
 	id := c.Param("id")
