361750f81eef9b67cd9b38c6931b5e5272c0c024
nothiaki <celsohenrique367@gmail.com>
2025-09-05 22:54:10 -0300

    docs: download blob todo and swag

--- api/handler/blob/downloadByID.go
+++ api/handler/blob/downloadByID.go
@@ -7,8 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary      Download blob
+// @Description  Download a single blob by using the bucket and id
+// @Accept       json
+// @Produce      application/octet-stream
+// @Param        Authorization header string true "Access token" default("")
+// @Param 			 bucket path string true "Bucket name"
+// @Param 			 id path string true "Blob id"
+// @Success      200 {object}
+// @Router       /blobs/download/{bucket}/{id} [get]
 func (bh *BlobHandler) DownloadByID(c *gin.Context) {
 	id := c.Param("id")
+	// TODO: take by id and bucket
 
 	if id == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "verify the id sent"})
