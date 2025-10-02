6a341f1ba7474b6746edf88c92190ee0cb439c1a
Celso Henrique Souza Silva <celsohenrique367@gmail.com>
2025-10-01 22:14:47 -0300

    rm: old repo

--- internal/repo/signatures/delete.go
+++
@@ -1,5 +0,0 @@
-package signatures
-
-func (ms *MapSignatures) Delete(key string) {
-	delete(ms.hm, key)
-}
--- internal/repo/signatures/set.go
+++
@@ -1,7 +0,0 @@
-package signatures
-
-import "github.com/blobtrtl3/trtl3/internal/domain"
-
-func (ms *MapSignatures) Set(key string, val domain.Signature) {
-	ms.hm[key] = val
-}
