d4d14138f8e23c2baa9ce0b78fc8d782583cd383
Celso Henrique Souza Silva <celsohenrique367@gmail.com>
2025-10-01 21:57:03 -0300

    chore: domain files

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -2,10 +2,18 @@ package domain
 
 import "time"
 
+// type BlobMeta struct { // TODO: rename
 type BlobInfo struct {
 	ID        string    `json:"id"`
 	Bucket    string    `json:"bucket"`
 	Mime      string    `json:"mime_type"`
 	Size      int64     `json:"size"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type Signature struct {
+	Bucket string
+	ID     string
+	TTL    time.Time
+	Once   bool // if once is true after one access should delete the signature
 }
